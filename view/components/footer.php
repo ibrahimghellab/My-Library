bba543887bfdc11a524bc37e5c2089c4b6b962af
ibrahimghellab <ghellabibrahim@gmail.com>
2024-12-16 10:24:43 +0100

    Add footer component

---
+++ view/components/footer.php
@@ -0,0 +1,21 @@
+<footer>
+    <p>© Tous droits réservés</p>
+    <div>
+        <a href="">FAQ</a>
+        <a href="">Terms of Service</a>
+        <a href="">Privacy Policy</a>
+        <a href="">Refund Policy</a>
+    </div>
+   <div>
+        <p>Newsletter</p>
+        <p>Subscribe to receive updates, access to exclusive deals, and more.</p>
+        <input type="email" placeholder="Enter your email">
+        <button>Subscribe</button>
+   </div>
+   <div>
+        <p>Follow us</p>
+        <a href="">Instagram</a>
+        <a href="">Twitter</a>
+        <a href="">TikTok</a>
+   </div>
+</footer>
