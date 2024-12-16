048cb7bebea8ea8d5e4caf61a6027b55b5cdf9d9
ibrahimghellab <ghellabibrahim@gmail.com>
2024-12-16 10:15:57 +0100

    Add navigation component

---
+++ view/components/nav.php
@@ -0,0 +1,11 @@
+<nav>
+    <ul>
+        <li>Home</li>
+        <li>Our products</li>
+        <li>Contact us</li>
+    </ul>
+    <ul>
+        <li>Account</li>
+        <li>Shopping cart</li>
+    </ul>
+</nav>
